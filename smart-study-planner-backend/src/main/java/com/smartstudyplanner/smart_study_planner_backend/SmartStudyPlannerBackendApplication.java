01beced6ae1eb37249d52bf67aa263ab66604ad6
sharma-mitneu <145163557+sharma-mitneu@users.noreply.github.com>
2025-03-23 17:11:39 -0400

    adding new files

---
+++ smart-study-planner-backend/src/main/java/com/smartstudyplanner/smart_study_planner_backend/SmartStudyPlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartstudyplanner.smart_study_planner_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStudyPlannerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartStudyPlannerBackendApplication.class, args);
+	}
+
+}
---
+++ smart-study-planner-backend/src/test/java/com/smartstudyplanner/smart_study_planner_backend/SmartStudyPlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartstudyplanner.smart_study_planner_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStudyPlannerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
